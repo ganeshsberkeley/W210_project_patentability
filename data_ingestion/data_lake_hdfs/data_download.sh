3f3b09e6ec198c73cbcdf91355b3660ad20e4d8c
root <root@ip-172-31-17-117.ec2.internal>
2017-10-15 21:53:53 +0000

    Updated data_download.sh to make it usable

--- data_ingestion/data_lake_hdfs/data_download.sh
+++ data_ingestion/data_lake_hdfs/data_download.sh
@@ -29,60 +29,60 @@ rm *.zip
 cd ../
 echo $pwd
 
+exit
+
 
 echo "# getting rid of header ..."
-tail -n +2 data_sets/2014/foreign_priority.csv > data_sets/2014/foreign_priority.csv
-tail -n +2 data_sets/2015/foreign_priority.csv > data_sets/2015/foreign_priority.csv
-tail -n +2 data_sets/2015/application_data.csv > data_sets/2015/application_data.csv
-tail -n +2 data_sets/2015/application_data.csv > data_sets/2015/application_data.csv
-tail -n +2 data_sets/2015/application_data.csv > data_sets/2015/application_data.csv
-tail -n +2 data_sets/2015/application_data.csv > data_sets/2015/application_data.csv
-tail -n +2 data_sets/2014/transactions.csv > data_sets/2014/transactions.csv
-tail -n +2 data_sets/2015/transactions.csv > data_sets/2015/transactions.csv
-tail -n +2 data_sets/2014/event_codes.csv > data_sets/2014/event_codes.csv
-tail -n +2 data_sets/2015/event_codes.csv > data_sets/2015/event_codes.csv
-tail -n +2 data_sets/2014/status_codes.csv > data_sets/2014/status_codes.csv
-tail -n +2 data_sets/2015/status_codes.csv > data_sets/2015/status_codes.csv
-tail -n +2 data_sets/2014/patent_claims_fulltext.csv > data_sets/2014/patent_claims_fulltext.csv
+tail -n+2 data_sets/2014/foreign_priority.csv		> data_sets/2014/foreign_priority_2014.csv
+tail -n+2 data_sets/2014/application_data.csv		> data_sets/2014/application_data_2014.csv
+tail -n+2 data_sets/2014/transactions.csv		> data_sets/2014/transactions_2014.csv
+tail -n+2 data_sets/2014/event_codes.csv		> data_sets/2014/event_codes_2014.csv
+tail -n+2 data_sets/2014/status_codes.csv		> data_sets/2014/status_codes_2014.csv
+tail -n+2 data_sets/2014/patent_claims_fulltext.csv 	> data_sets/2014/patent_claims_fulltext_2014.csv
+
+tail -n+2 data_sets/2015/foreign_priority.csv		> data_sets/2015/foreign_priority_2015.csv
+tail -n+2 data_sets/2015/application_data.csv		> data_sets/2015/application_data_2015.csv
+tail -n+2 data_sets/2015/transactions.csv		> data_sets/2015/transactions_2015.csv
+tail -n+2 data_sets/2015/event_codes.csv		> data_sets/2015/event_codes_2015.csv
+tail -n+2 data_sets/2015/status_codes.csv		> data_sets/2015/status_codes_2015.csv
 
 
 echo "# creating /user/root/w210/ folder in HDFS ..."
 hdfs dfs -rm -r -f /user/root/w210
 
-hdfs dfs -mkdir /user/root/w210
-hdfs dfs -mkdir /user/root/w210/foreign_priority
-hdfs dfs -mkdir /user/root/w210/application_data
-hdfs dfs -mkdir /user/root/w210/transactions
-hdfs dfs -mkdir /user/root/w210/event_codes
-hdfs dfs -mkdir /user/root/w210/status_codes
-hdfs dfs -mkdir /user/root/w210/full_text
+#hdfs dfs -mkdir /user/root/w210
+#hdfs dfs -mkdir /user/root/w210/foreign_priority
+#hdfs dfs -mkdir /user/root/w210/application_data
+#hdfs dfs -mkdir /user/root/w210/transactions
+#hdfs dfs -mkdir /user/root/w210/event_codes
+#hdfs dfs -mkdir /user/root/w210/status_codes
+#hdfs dfs -mkdir /user/root/w210/full_text
 
-hdfs dfs -mkdir /user/root/w210/foreign_priority/2014
-hdfs dfs -mkdir /user/root/w210/application_data/2014
-hdfs dfs -mkdir /user/root/w210/transactions/2014
-hdfs dfs -mkdir /user/root/w210/event_codes/2014
-hdfs dfs -mkdir /user/root/w210/status_codes/2014
-hdfs dfs -mkdir /user/root/w210/full_text/2014
+hdfs dfs -mkdir -p /user/root/w210/foreign_priority/2014
+hdfs dfs -mkdir -p /user/root/w210/application_data/2014
+hdfs dfs -mkdir -p /user/root/w210/transactions/2014
+hdfs dfs -mkdir -p /user/root/w210/event_codes/2014
+hdfs dfs -mkdir -p /user/root/w210/status_codes/2014
+hdfs dfs -mkdir -p /user/root/w210/full_text/2014
 
 
-hdfs dfs -mkdir /user/root/w210/foreign_priority/2015
-hdfs dfs -mkdir /user/root/w210/application_data/2015
-hdfs dfs -mkdir /user/root/w210/transactions/2015
-hdfs dfs -mkdir /user/root/w210/event_codes/2015
-hdfs dfs -mkdir /user/root/w210/status_codes/2015
-hdfs dfs -mkdir /user/root/w210/full_text/2015
+hdfs dfs -mkdir -p /user/root/w210/foreign_priority/2015
+hdfs dfs -mkdir -p /user/root/w210/application_data/2015
+hdfs dfs -mkdir -p /user/root/w210/transactions/2015
+hdfs dfs -mkdir -p /user/root/w210/event_codes/2015
+hdfs dfs -mkdir -p /user/root/w210/status_codes/2015
+hdfs dfs -mkdir -p /user/root/w210/full_text/2015
 
 echo "#Putting Files in Data Lake"
-hdfs dfs -put data_sets/2015/foreign_priority.csv /user/root/w210/foreign_priority/2015/foreign_priority.csv
-hdfs dfs -put data_sets/2015/application_data.csv /user/root/w210/application_data/2015/application_data.csv
-hdfs dfs -put data_sets/2015/transactions.csv /user/root/w210/transactions/2015/transactions.csv
-hdfs dfs -put data_sets/2015/event_codes.csv /user/root/w210/event_codes/2015/event_codes.csv
-hdfs dfs -put data_sets/2015/status_codes.csv /user/root/w210/status_codes/2015/status_codes.csv
+hdfs dfs -put data_sets/2014/foreign_priority_2014.csv /user/root/w210/foreign_priority/2014/foreign_priority_2014.csv
+hdfs dfs -put data_sets/2014/application_data_2014.csv /user/root/w210/application_data/2014/application_data_2014.csv
+hdfs dfs -put data_sets/2014/transactions_2014.csv /user/root/w210/transactions/2014/transactions_2014.csv
+hdfs dfs -put data_sets/2014/event_codes_2014.csv /user/root/w210/event_codes/2014/event_codes_2014.csv
+hdfs dfs -put data_sets/2014/status_codes_2014.csv /user/root/w210/status_codes/2014/status_codes_2014.csv
+hdfs dfs -put data_sets/2014/patent_claims_fulltext_2014.csv /user/root/w210/full_text/2014/patent_claims_fulltext_2014.csv
 
-hdfs dfs -put data_sets/2014/foreign_priority.csv /user/root/w210/foreign_priority/2014/foreign_priority.csv
-hdfs dfs -put data_sets/2014/application_data.csv /user/root/w210/application_data/2014/application_data.csv
-hdfs dfs -put data_sets/2014/transactions.csv /user/root/w210/transactions/2014/transactions.csv
-hdfs dfs -put data_sets/2014/event_codes.csv /user/root/w210/event_codes/2014/event_codes.csv
-hdfs dfs -put data_sets/2014/status_codes.csv /user/root/w210/status_codes/2014/status_codes.csv
-
-hdfs dfs -put data_sets/2014/patent_claims_fulltext.csv /user/root/w210/full_text/2014/patent_claims_fulltext.csv
+hdfs dfs -put data_sets/2015/foreign_priority_2015.csv /user/root/w210/foreign_priority/2015/foreign_priority_2015.csv
+hdfs dfs -put data_sets/2015/application_data_2015.csv /user/root/w210/application_data/2015/application_data_2015.csv
+hdfs dfs -put data_sets/2015/transactions_2015.csv /user/root/w210/transactions/2015/transactions_2015.csv
+hdfs dfs -put data_sets/2015/event_codes_2015.csv /user/root/w210/event_codes/2015/event_codes_2015.csv
+hdfs dfs -put data_sets/2015/status_codes_2015.csv /user/root/w210/status_codes/2015/status_codes_2015.csv
