5aa634d6c1a5af27d5c525377d921bce27a1e4c5
root <root@ip-172-31-17-117.ec2.internal>
2017-10-29 20:20:14 +0000

    Python scripts for scafolding full text data

--- scripts/process_full.py
+++ scripts/process_full.py
@@ -7,9 +7,8 @@ from bs4 import BeautifulSoup
 import time
 import os, re
 import sys
-import nltk
-from nltk.corpus import stopwords
-from nltk.stem.porter import *
+
+import unicodedata
 
 
 def getClaimsAndClasses(inFile):
@@ -23,42 +22,51 @@ def getClaimsAndClasses(inFile):
 	soup = BeautifulSoup(f, 'xml')
 	if soup:
 		section = soup.find('section')
-		# cs = soup.find('class')
+
 		if section:# and cs and cs is not '':
 			section = section.text
 			if section == 'H' or section == 'G':
 				claims = soup.find_all('claim-text')
+
+
 				w1 = open(sys.path[0]+'/claims-full/'+inF+'_.txt', 'w')
-				# w2 = open(sys.path[0]+'/'+inF+'_.txt', 'w')
 
 				for claim in claims:
-					# text = [stem(regex.sub(' ', word)) for word in claim.text.lower().split() if word not in cached]
-					# text = [stem(word) for word in text if ''.join(word.split()) not in cached]
-					# text = " ".join(text)
-					w1.write(claim.text)
-					# w1.write(claim.text)
-
-			# w2.write(section.text + " " + cs.text)
-
-			# if not os.path.exists('search/'+section.text+cs.text):
-			# 	os.makedirs('search/'+section.text+cs.text)
-			# w3 = open(sys.path[0]+'/search/'+section.text+cs.text+'/'+inF+'.txt', 'w')
-			# for claim in claims:
-			# 	w3.write(claim.text)
-
+					w1.write(claim.text.encode("utf-8"))
 				w1.close()
-			# w2.close()
-			# w3.close()
+
+def getPriorAndClasses(inFile):
+
+	e = xml.etree.ElementTree.parse(inFile).getroot()
+
+	split = inFile.split('/')
+	inF = split[len(split) - 1]
+
+	f = open(inFile, 'r')
+	soup = BeautifulSoup(f, 'xml')
+	if soup:
+		reference = soup.find('publication-reference')
 
 
-# getClaimsAndClasses('iptest.xml')
+		if reference:# and cs and cs is not '':
+			priorart = reference.find_all('doc-number')
+			w1 = open(sys.path[0]+'/priorart/'+inF+'_.txt', 'w')
+			for ref_num in priorart:
+				w1.write(ref_num.text.encode("utf-8"))
+				w1.write(" ")
+			w1.close()
 
-files = glob.glob(sys.path[0]+"/xml-appa/*.xml")
+
+files = glob.glob(sys.path[0]+"/xml-split/*.xml")
+
 i = 0
 for fil in files:
 	split = fil.split('/')
 	last = split[len(split) - 1]
-	getClaimsAndClasses(fil)
+#	getClaimsAndClasses(fil)
+
+	getPriorAndClasses(fil)
+
 	if i % 500 == 0:
 		print('finished', i)
 	i += 1
