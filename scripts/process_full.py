1cc906e55e19ab6cd5d10bde98a02f4f42365f74
root <root@ip-172-31-17-117.ec2.internal>
2017-10-29 23:04:44 +0000

    Updated the process_full.py script

--- scripts/process_full.py
+++ scripts/process_full.py
@@ -10,14 +10,29 @@ import sys
 
 import unicodedata
 
-
-def getClaimsAndClasses(inFile):
+# Extract the claims text
+def getClaimsAndClasses(inFile, w1):
 
 	e = xml.etree.ElementTree.parse(inFile).getroot()
 
-	split = inFile.split('/')
-	inF = split[len(split) - 1]
 
+	# Open the input file
+	f = open(inFile, 'r')
+	soup = BeautifulSoup(f, 'xml')
+	if soup:
+		pub_no = soup.find('publication-reference') 
+
+		if pub_no:# and cs and cs is not '':
+			publication = pub_no.find_all('doc-number')
+			for pub_num in publication:
+				doc_num = pub_num.text.encode("utf-8")
+				# Remove all special charecters
+				doc_num = re.sub('[^A-Za-z0-9]+', '', doc_num)
+				w1.write(doc_num)
+				w1.write(",")
+	f.close()
+
+	# Reopen it for claims information
 	f = open(inFile, 'r')
 	soup = BeautifulSoup(f, 'xml')
 	if soup:
@@ -28,45 +43,57 @@ def getClaimsAndClasses(inFile):
 			if section == 'H' or section == 'G':
 				claims = soup.find_all('claim-text')
 
-
-				w1 = open(sys.path[0]+'/claims-full/'+inF+'_.txt', 'w')
-
+				w1.write("\"") ;
 				for claim in claims:
-					w1.write(claim.text.encode("utf-8"))
-				w1.close()
+					claim_txt = claim.text.encode("utf-8")
+					claim_txt = re.sub('[\n\r]+', '', claim_txt)
+					w1.write(claim_txt)
+				w1.write("\"") ;
+				w1.write(",")
+	f.close()
 
-def getPriorAndClasses(inFile):
-
-	e = xml.etree.ElementTree.parse(inFile).getroot()
-
-	split = inFile.split('/')
-	inF = split[len(split) - 1]
-
+	# Reopen it for priorart information
 	f = open(inFile, 'r')
 	soup = BeautifulSoup(f, 'xml')
 	if soup:
-		reference = soup.find('publication-reference')
-
+		reference = soup.find('us-references-cited')
 
 		if reference:# and cs and cs is not '':
 			priorart = reference.find_all('doc-number')
-			w1 = open(sys.path[0]+'/priorart/'+inF+'_.txt', 'w')
 			for ref_num in priorart:
-				w1.write(ref_num.text.encode("utf-8"))
+				pri_info = ref_num.text.encode("utf-8")
+				pri_info = re.sub('[^A-Za-z0-9]+', '', pri_info)
+				w1.write(pri_info)
 				w1.write(" ")
-			w1.close()
+
+	w1.write("\n")
+	f.close()
+
 
 
 files = glob.glob(sys.path[0]+"/xml-split/*.xml")
 
 i = 0
+
 for fil in files:
 	split = fil.split('/')
 	last = split[len(split) - 1]
-#	getClaimsAndClasses(fil)
 
-	getPriorAndClasses(fil)
+	# Open output file
+	if ( i == 0 ):
+		split = fil.split('/')
+		inF = split[len(split) - 1]
+		split = inF.split('.') 
+		inF = split[0]
+		w1 = open(sys.path[0]+'/scrape/'+inF+'_xml.csv', 'w')
+		w1.write("pub_no,claims_txt,prior_art\n") ;
+
+	getClaimsAndClasses(fil, w1)
 
 	if i % 500 == 0:
 		print('finished', i)
+
+	if (i == len(files)-1 ):
+		w1.close()
+
 	i += 1
